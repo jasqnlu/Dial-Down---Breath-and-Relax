4c7bc4b181f08e39f09d616a459eae675eb3c186
jasqnlu <07jasonlu2010@gmail.com>
2026-08-16 14:29:42 -0700

    refactor(routines): remove isPublic/borrowCount/authorID/authorName from Routine

--- Breath - Relax & Stretch/Models/Routine.swift
+++ Breath - Relax & Stretch/Models/Routine.swift
@@ -7,31 +7,19 @@ final class Routine {
     var uuid: UUID = UUID()
     var name: String = ""
     var exerciseIDs: [UUID] = []
-    var authorID: String? = nil
-    var authorName: String? = nil
     var borrowedFromID: UUID? = nil
-    var isPublic: Bool = false
-    var borrowCount: Int = 0
     var createdAt: Date = Date()
 
     init(
         uuid: UUID = UUID(),
         name: String,
         exerciseIDs: [UUID] = [],
-        authorID: String? = nil,
-        authorName: String? = nil,
-        borrowedFromID: UUID? = nil,
-        isPublic: Bool = false,
-        borrowCount: Int = 0
+        borrowedFromID: UUID? = nil
     ) {
         self.uuid = uuid
         self.name = name
         self.exerciseIDs = exerciseIDs
-        self.authorID = authorID
-        self.authorName = authorName
         self.borrowedFromID = borrowedFromID
-        self.isPublic = isPublic
-        self.borrowCount = borrowCount
         self.createdAt = Date()
     }
 }
